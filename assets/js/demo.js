8ac152cf5de5748177845d1097bbd497a1bc2cf1
Loren <lorengrx@gmail.com>
2014-08-01 13:27:25 +0200

    Modification of Demo CSS

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,5 +1,7 @@
-angular.module("flatstrapDemo",["flatstrap","ui.bootstrap"])
-       .controller("flatgridBasic",function($scope,$q){
+angular.module("flatstrapDemo",[
+	"flatstrap",
+	"ui.bootstrap"
+]).controller("flatgridController",function($scope,$q){
 
 	$scope.flatgridOptsBasic = {
 		"rows" : [
@@ -56,6 +58,66 @@ angular.module("flatstrapDemo",["flatstrap","ui.bootstrap"])
 			,title  : "Basic Demo"
 		}
 	}
+
+	$scope.flatgridOptsFormTypes = {
+		"rows" : [
+			{
+				"Id"      : "1",
+				"Event"   : "Jazz Festival",
+				"SoldOut" : true,
+				"Date"    : "2012-09-02T09:11:43Z"
+			},
+			{
+				"Id"      : "2",
+				"Event"   : "Annual Veterans Reunion",
+				"SoldOut" : false,
+				"Date"    : "2013-01-19T08:00:00Z"
+			},
+			{
+				"Id"      : "3",
+				"Event"   : "Salsa Dancing Show",
+				"SoldOut" : false,
+				"Date"    : "2013-12-24T00:00:00Z"
+			},
+			{
+				"Id"      : "4",
+				"Event"   : "Rock Festival",
+				"SoldOut" : true,
+				"Date"    : "2013-08-10T11:00:00Z"
+			}
+
+		],
+		"columns" : [
+			{
+				Name       : "Event",
+				Label      : "Event Name",
+				Sortable   : true,
+				AutoFocus  : true,
+				Editable   : false,
+				Form       : "input"
+			},
+			{
+				Name       : "Date",
+				Label      : "Event Date",
+				Sortable   : true,
+				Form       : "datetime"
+			},
+			{
+				Name       : "SoldOut",
+				Label      : "Sold Out",
+				Sortable   : true,
+				Form       : "checkbox"
+			}
+		],
+		options : {
+			 onsave : "saveItem"
+			,onadd : "addItem"
+			,ondelete : "deleteItem"
+			,title    : "Form Types Demo"
+			,controls : false
+		}
+	}
+
 
     $scope.addItem = function(pkg) {
         var p = $q.defer();
